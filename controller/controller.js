ef05555c6ca3a65db28cc726474ec55f3b0b01d2
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2016-06-16 12:30:22 -0400

    adding tims

--- controller/controller.js
+++ controller/controller.js
@@ -1,3 +1,10 @@
 router.get('/tims', function(req,res){
 	res.send('david');
-})
+	console.log('tacos');
+});
+
+
+router.get('david',function(req.res){
+	res.send('tims')
+	console.log('pizza')
+});
