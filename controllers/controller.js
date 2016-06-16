11ea803b2e39e103b313f38590d3ac7242e75a4d
Shiiba <kshiiba@gmail.com>
2016-06-16 12:36:40 -0400

    fixing merge conflicts

--- controllers/controller.js
+++ controllers/controller.js
@@ -9,4 +9,15 @@ router.get('/joe', function(req,res){
   res.send('Joe');
 });
 
+router.get('/tims', function(req,res){
+  res.send('david');
+  console.log('tacos');
+});
+
+
+router.get('david',function(req.res){
+  res.send('tims')
+  console.log('pizza')
+});
+
 module.exports = router;
